cd50761558e7631b9da43ef0bb0464c5be55e23b
TraianV <61265978+TraianV@users.noreply.github.com>
2020-11-18 20:43:31 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,11 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'temaphp');
 
-/* Note: Replace the credentials according to your MySQL server setting before testing this code, 
-for example, replace the database name 'demo' with your own database name, replace username 'root' with your own database username, 
-specify database password if there's any.*/
- 
-/* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
